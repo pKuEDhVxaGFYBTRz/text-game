aece340369ba5d3a923a2750599376c2e453e3dc
Jake <Jake@DESKTOP-77AIUI2>
2022-03-04 11:27:44 +0000

    Add project files.

---
+++ text-game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace text_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
