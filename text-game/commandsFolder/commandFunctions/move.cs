2be27cd2931779b1ebbafc18624df34d3b06436b
Jake <s21000000300@leggott.ac.uk>
2022-03-10 22:37:01 +0000

    committing changes

---
+++ text-game/commandsFolder/commandFunctions/move.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using text_game.game;
+
+namespace text_game
+{
+    internal static class commandFunctions
+    {
+        public static void left(player player)
+        {
+            player.position.x--;
+            if (player.position.x < 0) { Console.WriteLine("you have fallen off the edge of the flat earth."); player.dead = true; }
+        }
+
+        public static void right(player player)
+        {
+            player.position.x++;
+            if (player.position.x > 4) { Console.WriteLine("you have fallen off the edge of the flat earth."); player.dead = true; }
+        }
+
+        public static void forwards(player player)
+        {
+            player.position.y++;
+            if (player.position.y < 0) { Console.WriteLine("you have fallen off the edge of the flat earth."); player.dead = true; }
+        }
+
+        public static void backwards(player player)
+        {
+            player.position.x--;
+            if (player.position.x > 4) { Console.WriteLine("you have fallen off the edge of the flat earth."); player.dead = true; }
+        }
+    }
+}
