039c01e27079d674b949bbb33c9d64ea8cd92af1
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2022-03-22 11:32:24 +0000

    stuff

---
+++ text-game/commandsFolder/help.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using text_game.game;
+using text_game.game.locations;
+
+namespace text_game
+{
+    internal static class generalCommandFunctions
+    {
+        internal static void look(player p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void help(player p)
+        {
+            Console.WriteLine("opening the help file in your default editor");
+            System.Diagnostics.Process.Start("help.md");
+        }
+    }
+}
---
+++ text-game/game/locations/locationEnums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_game.game.locations
+{
+    static public class locationEnums
+    {
+        public enum locations
+        {
+            dessert,
+            woodsCenter,
+            woodsOuter,
+
+        }
+
+        static public Tile lookupLocation(locations locationsParam)
+        {
+            switch (locationsParam)
+            {
+                case locations.dessert: return new desert();
+                case locations.woodsCenter: return new woodsCenter();
+                case locations.woodsOuter: return new woodsOuter();
+            }
+            return null;
+        }
+    }
+}
